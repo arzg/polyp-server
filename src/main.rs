c4f60e449e636c5a1879a15d9b6de7caff6d8393
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-22 23:02:20 +1100

    Switch back to JSON over stdio

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,33 @@
-use anyhow::{anyhow, Context};
-use polyp::{ServerMsg, Ui, UserInput};
-use std::env;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener};
-use std::process::Command;
-use tungstenite::Message;
-use url::Url;
+use polyp::{Ui, UserInput};
+use std::io::{self, BufRead, BufReader, Write};
+use std::process::{Command, Stdio};
 
 fn main() -> anyhow::Result<()> {
-    let (mut client_websocket, _) = {
-        let client_socket_addr = env::args()
-            .nth(1)
-            .ok_or_else(|| anyhow!("expected argument of client socket address"))?;
-
-        let client_websocket_url = Url::parse(&format!("ws://{}", client_socket_addr))?;
-
-        tungstenite::client::connect(client_websocket_url).context("server")?
-    };
-
-    println!("polyp-server: connected to client\r");
-
-    let socket_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 16180);
-
-    Command::new("kon-polyp")
-        .arg(socket_addr.to_string())
+    let kon = Command::new("kon-polyp")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
         .spawn()?;
 
-    let mut processlet_websocket = {
-        let listener = TcpListener::bind(socket_addr)?;
-        let stream = listener.incoming().next().unwrap()?;
-
-        tungstenite::server::accept(stream)?
-    };
-    println!("polyp-server: connected to processlet\r");
+    let mut kon_stdin = kon.stdin.unwrap();
+    let mut kon_stdout = BufReader::new(kon.stdout.unwrap());
 
     loop {
-        let user_input = match client_websocket.read_message()? {
-            Message::Binary(json) => serde_json::from_slice(&json)?,
-            Message::Close(_) => {
-                processlet_websocket.close(None)?;
-                return Ok(());
-            }
-            _ => unreachable!(),
-        };
+        let mut line = String::new();
+        io::stdin().read_line(&mut line)?;
+        let user_input: UserInput = serde_json::from_str(&line)?;
+        eprintln!("polyp-server: got user input {:?}\r", user_input);
 
-        let UserInput::PressedKey(c) = user_input;
-        println!("polyp-server: client says user pressed ‘{:?}’\r", c);
+        serde_json::to_writer(&kon_stdin, &user_input)?;
+        kon_stdin.write_all(b"\n")?;
+        eprintln!("polyp-server: forwarded user input\r");
 
-        processlet_websocket.write_message(Message::Binary(serde_json::to_vec(
-            &UserInput::PressedKey(c),
-        )?))?;
-        println!("polyp-server: forwarded on user input to processlet\r");
+        let mut line = String::new();
+        kon_stdout.read_line(&mut line)?;
+        let ui: Ui = serde_json::from_str(&line)?;
+        eprintln!("polyp-server: received UI {:?}\r", ui);
 
-        let processlet_msg = processlet_websocket.read_message()?;
-        println!("polyp-server: read UI from processlet\r");
-
-        if let Message::Binary(json) = processlet_msg {
-            let processlet_ui: Ui = serde_json::from_slice(&json)?;
-
-            let server_msg = match processlet_ui {
-                Ui::Value(value) => ServerMsg::NewText(format!("{:#?}", value)),
-                Ui::TextField {
-                    current_text,
-                    cursor_idx,
-                } => {
-                    let with_cursor = {
-                        let mut s = current_text;
-                        s.insert(cursor_idx, '|');
-                        s
-                    };
-
-                    ServerMsg::NewText(with_cursor)
-                }
-            };
-
-            let serialized_server_msg = serde_json::to_vec(&server_msg)?;
-
-            client_websocket.write_message(Message::Binary(serialized_server_msg))?;
-            println!("polyp-server: sent new text to client\r");
-        } else {
-            unreachable!()
-        }
+        io::stdout().write_all(&serde_json::to_vec(&ui)?)?;
+        io::stdout().write_all(b"\n")?;
+        eprintln!("polyp-server: forwarded UI\r");
     }
 }
