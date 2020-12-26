a388d590f32ba8585f108b39a5b25d559ff451f5
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-26 21:20:57 +1100

    Migrate from polyp::protocol to jsonl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use polyp::protocol::Connection;
+use jsonl::Connection;
 use polyp::{ProcessletMsg, ServerMsg, Ui, UserInput};
 use std::process::{Command, Stdio};
 
@@ -8,11 +8,11 @@ fn main() -> anyhow::Result<()> {
         .stdout(Stdio::piped())
         .spawn()?;
 
-    let mut client_connection = Connection::new_from_current_process();
+    let mut client_connection = Connection::new_from_stdio();
     let mut kon_connection = Connection::new_from_child(&mut kon).unwrap();
 
     loop {
-        let server_msg: ServerMsg = client_connection.recv_message()?;
+        let server_msg: ServerMsg = client_connection.read()?;
         eprintln!("polyp-server: got server message {:?}\r", server_msg);
 
         let processlet_msg = match server_msg {
@@ -21,20 +21,20 @@ fn main() -> anyhow::Result<()> {
             }
             ServerMsg::Shutdown => {
                 eprintln!("polyp-server: shutting down...\r");
-                kon_connection.send_message(&ProcessletMsg::Shutdown)?;
+                kon_connection.write(&ProcessletMsg::Shutdown)?;
                 kon.wait()?;
 
                 return Ok(());
             }
         };
 
-        kon_connection.send_message(&processlet_msg)?;
+        kon_connection.write(&processlet_msg)?;
         eprintln!("polyp-server: forwarded user input\r");
 
-        let ui: Ui = kon_connection.recv_message()?;
+        let ui: Ui = kon_connection.read()?;
         eprintln!("polyp-server: received UI {:?}\r", ui);
 
-        client_connection.send_message(&ui)?;
+        client_connection.write(&ui)?;
         eprintln!("polyp-server: forwarded UI\r");
     }
 }
